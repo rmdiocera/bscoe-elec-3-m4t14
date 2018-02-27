ad91650e0442a56e0c56b650a9a1b0576474e1ae
rmdiocera <34466258+rmdiocera@users.noreply.github.com>
2018-02-27 15:03:12 +0800

    Added script for enemy explosion on bullet hit

---
+++ Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour {
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] int hits = 3;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        AddBoxCollider();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hits = hits - 1;
+        // todo consider hit FX
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
