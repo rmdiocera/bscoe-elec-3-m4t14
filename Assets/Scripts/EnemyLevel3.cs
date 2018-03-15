087ef1adf7d7b51782ad7784c5720caf428b1e30
rmdiocera <34466258+rmdiocera@users.noreply.github.com>
2018-03-15 15:20:43 +0800

    Added scripts for different enemy ship and scoring

---
+++ Assets/Scripts/EnemyLevel3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLevel3 : MonoBehaviour
+{
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] int hits = 7;
+    [SerializeField] int hitScore = 10;
+
+    GameScore scoreBoard;
+
+    // Use this for initialization
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<GameScore>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.OnEnemyHit(hitScore);
+        hits = hits - 1;
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyLevel5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLevel5 : MonoBehaviour
+{
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] int hits = 11;
+    [SerializeField] int hitScore = 10;
+
+    GameScore scoreBoard;
+
+    // Use this for initialization
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<GameScore>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.OnEnemyHit(hitScore);
+        hits = hits - 1;
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
+
